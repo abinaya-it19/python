5a654c154ba897f04c2242f3f62f65ef526252c4
abinaya-19 <abinayak.it19@bitsathy.ac.in>
2019-10-22 16:01:47 +0530

    second commit

---
+++ rectangle.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+    int main(){
+                   int width, length, area;
+                   printf("Give the width of Rectangle : ");
+                   scanf("%d", &width);
+                   printf("\n Give the Length of Rectangle : ");
+                   scanf("%d", &length);
+                   area = length * width;
+                   printf("\n\n The Area of Rectangle is : %d", area);
+    return (0);
+    }
