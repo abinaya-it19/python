5b301627a8be8bb20dc9107b503ec3ab750c05ce
abinaya-it19 <abinayak.it19@bitsathy.ac.in>
2019-10-22 15:00:39 +0530

    second commit

---
+++ ari.py
@@ -0,0 +1,11 @@
+a=45
+b=10
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print (c)
+print (d)
+print (e)
+print (f)
+
---
+++ wel.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+char str[20];
+scanf("%s",str);
+printf("welcome %s",str[20]);
+return 0;
+}
